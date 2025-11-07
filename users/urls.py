3ac0b01c069a236da0ef527fa088049aa3a90b6a
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-11-07 10:21:09 +0200

    Add URL pattern for user profile view

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,15 @@
+from django.urls import path
+from . import views
 
+# Це потрібно, щоб Django розрізняв, наприклад, 'store:profile' і 'users:profile'
+app_name = 'users'
+
+urlpatterns = [
+    # Ми створимо 'profile_view' у наступному кроці
+    
+    # URL буде: /users/profile/
+    path('profile/', views.profile_view, name='profile'),
+    
+    # Ми могли б додати тут сторінку реєстрації, 
+    # але Django вже має вбудовану, тож не будемо ускладнювати.
+]

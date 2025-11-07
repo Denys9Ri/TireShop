d05ce88351eece4df3f34ca2462d274ed707caad
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-11-07 10:22:20 +0200

    Implement profile view with order retrieval
    Added a profile view to display user's orders.

--- users/views.py
+++ users/views.py
@@ -1,1 +1,17 @@
+from django.shortcuts import render
+# Це "фейс-контроль" від Django. Не пустить анонімних користувачів.
+from django.contrib.auth.decorators import login_required
+# Нам потрібна модель 'Order' з магазину, щоб знайти замовлення
+from store.models import Order
 
+# --- 1. СТОРІНКА КАБІНЕТУ ---
+
+@login_required # <-- той самий "фейс-контроль"
+def profile_view(request):
+    # Шукаємо в базі всі замовлення, де поле 'customer'
+    # дорівнює поточному залогіненому користувачу (request.user).
+    # Сортуємо від найновішого до найстарішого.
+    orders = Order.objects.filter(customer=request.user).order_by('-created_at')
+    
+    # "Малюємо" сторінку 'profile.html', передаючи їй список замовлень
+    return render(request, 'users/profile.html', {'orders': orders})

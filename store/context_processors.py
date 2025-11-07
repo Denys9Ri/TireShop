632990ad45806d540c37a81dff4db7ec47869a06
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-11-07 09:36:48 +0200

    Implement cart context processor
    Add a context processor to provide cart object in templates.

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,1 +1,7 @@
+from .cart import Cart
 
+def cart(request):
+    # Ця функція просто бере поточний запит (request),
+    # створює об'єкт кошика (з сесії)
+    # і повертає його у словнику під ключем 'cart'.
+    return {'cart': Cart(request)}

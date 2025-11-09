77b9006fe042b4e3ddac2b55fd6b12b0141a1ca8
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-11-09 18:22:13 +0200

    Update settings.py

--- TireShop/settings.py
+++ TireShop/settings.py
@@ -82,7 +82,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

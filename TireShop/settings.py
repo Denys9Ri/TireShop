51fb983664058b7eba1928d7be646da6d87394e1
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-11-24 09:59:05 +0200

    Update settings.py

--- TireShop/settings.py
+++ TireShop/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'TireShop.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600 
+        conn_max_age=0 
     )
 }
 

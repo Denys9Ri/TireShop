a48c1edd3c25039a6c631500ad006eda446dc6be
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-12-02 20:10:14 +0200

    Remove old registration path from urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # Старий шлях до кабінету
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.profile_edit_view, name='profile_edit'),
-
-    # НОВИЙ шлях до реєстрації
-    # (views.RegisterView.as_view() - так викликаються Класи-інженери)
-    path('register/', views.RegisterView.as_view(), name='register'),
 ]

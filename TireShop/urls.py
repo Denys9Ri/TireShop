958a90c3555c97628696e336712b28a7648e61cd
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-12-02 20:03:53 +0200

    Update URLs to include warranty view and fix robots.txt

--- TireShop/urls.py
+++ TireShop/urls.py
@@ -3,47 +3,41 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve 
 from django.contrib.sitemaps.views import sitemap
-from django.http import HttpResponse # <--- ЦЕ ВАЖЛИВО
+from django.http import HttpResponse
 
-# Імпорт карт сайту
+# Імпорти для головних сторінок
 from store.sitemaps import ProductSitemap, StaticViewSitemap
-from store.views import catalog_view, contacts_view, delivery_payment_view
+from store.views import catalog_view, contacts_view, delivery_payment_view, warranty_view
 
 sitemaps = {
     'products': ProductSitemap,
     'static': StaticViewSitemap,
 }
 
-# --- ФУНКЦІЯ ROBOTS.TXT (ПРЯМО В КОДІ) ---
 def robots_txt(request):
-    content = """User-agent: *
-Allow: /
-
-Disallow: /admin/
-Disallow: /users/
-Disallow: /accounts/
-Disallow: /store/cart/
-Disallow: /store/checkout/
-
-Sitemap: https://r16.com.ua/sitemap.xml
-"""
+    content = "User-agent: *\nAllow: /\n\nSitemap: https://r16.com.ua/sitemap.xml"
     return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Головні сторінки
     path('', catalog_view, name='catalog'),
     path('contacts/', contacts_view, name='contacts'),
     path('delivery-and-payment/', delivery_payment_view, name='delivery_payment'),
+    path('warranty-and-returns/', warranty_view, name='warranty'),
+
+    # Підключення додатків (ТУТ БУЛА ПОМИЛКА, ТЕПЕР ВИПРАВЛЕНО)
     path('store/', include('store.urls')),
     path('users/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')), 
     
-    # --- SEO ---
+    # SEO
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('robots.txt', robots_txt), # <--- ТУТ ВИКЛИКАЄМО ФУНКЦІЮ
+    path('robots.txt', robots_txt),
 ]
 
-# --- MEDIA FILES ---
+# Медіа файли на сервері
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,

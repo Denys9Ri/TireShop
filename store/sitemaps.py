f6e85a6b166cadde1f83fb5dfd5f3d38d2564337
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2025-12-23 20:25:39 +0200

    Update sitemaps.py

--- store/sitemaps.py
+++ store/sitemaps.py
@@ -1,30 +1,46 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import Product
-
-class ProductSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.9
-
-    def items(self):
-        # Повертаємо в карту тільки товари, які є в наявності
-        return Product.objects.filter(stock_quantity__gt=0)
-
-    def location(self, obj):
-        return reverse('store:product_detail', args=[obj.id])
+from .models import Product, Brand
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.6
-    changefreq = 'weekly'
+    """Статичні сторінки: Головна, Каталог, Контакти..."""
+    priority = 0.5
+    changefreq = 'daily'
 
     def items(self):
-        # Тут перелічені назви (name='...') з вашого urls.py
+        # 🔥 ВИПРАВЛЕННЯ: Додали префікс 'store:' до всіх назв
         return [
-            'catalog',
-            'contacts',
-            'delivery_payment',
-            'warranty',  # Нова сторінка гарантії
+            'store:catalog',
+            'store:about',
+            'store:contacts',
+            'store:delivery_payment',
+            'store:warranty',
+            'store:faq',
         ]
 
     def location(self, item):
         return reverse(item)
+
+class ProductSitemap(Sitemap):
+    """Сторінки товарів"""
+    priority = 0.8
+    changefreq = 'weekly'
+
+    def items(self):
+        # 🔥 ВИПРАВЛЕННЯ: Додали .order_by('id'), щоб прибрати Warning
+        return Product.objects.filter(stock_quantity__gt=0).order_by('id')
+
+    def location(self, obj):
+        return reverse('store:product_detail', args=[obj.slug])
+
+class BrandSitemap(Sitemap):
+    """🔥 НОВЕ: Сторінки брендів (Aplus, Michelin...)"""
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return Brand.objects.all().order_by('name')
+
+    def location(self, obj):
+        # Генеруємо посилання типу /shiny/brendy/aplus/
+        return reverse('store:brand_landing', args=[obj.slug])

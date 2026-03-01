1e562c21888aa3946e781c9f9ba420da98b8ae45
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-03-01 17:32:23 +0200

    Update settings.py

--- TireShop/settings.py
+++ TireShop/settings.py
@@ -87,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'uk-ua'
+LANGUAGE_CODE = 'uk'
 TIME_ZONE = 'Europe/Kyiv'
 USE_I18N = True
 USE_TZ = True
